c12cb5ad834316b7b73dc765cd0e23b04cb58be9
Richard Alison <richardalison86@gmail.com>
2019-12-14 00:55:26 +0700

    Change iterations to 1

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,8 @@ int main()
 {
     srand(time(NULL));
     AI *ai = new AI(4, 0.0001);
-    for(int i = 0; i < 1000; ++i)
-    {
-        Game::reset();
-        Game::train(ai);
-    }
+    Game::reset();
+    Game::train(ai);
+    delete ai;
     return 0;
 }

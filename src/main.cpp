18b53d27598acee235b1e8e6c2532558eaca90ba
Richard Alison <richardalison86@gmail.com>
2020-01-09 03:04:12 +0700

    Print number of iterations

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 int main()
 {
     AI *ai = new AI(4, 1e-6);
+    int iter = 0;
     while(true)
     {
+        std::cout << "\n\nIteration #" << ++iter << std::endl;
         srand(0);
         Game::reset();
         Game::train(ai);
